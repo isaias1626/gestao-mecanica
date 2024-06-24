85f0862bf61a477eb0e549d902475c86262a9fea
¨isaias <isaiasrocha1626@gmail.com>
2024-06-24 14:45:26 -0300

    feat: add functions delete and adition

--- app/dashboard/gestao-de-pecas/page.tsx
+++ app/dashboard/gestao-de-pecas/page.tsx
@@ -1,88 +1,160 @@
+"use client";
+
 import HeaderPage from '@/components/header/header';
-import React from 'react';
+import React, { useState } from 'react';
+
+type Peca = {
+    nomePeca: string;
+    numeroLote: string;
+    precoCusto: string;
+    precoVenda: string;
+    quantidadeEstoque: string;
+};
 
 const Pecas = () => {
+    const [nomePeca, setNomePeca] = useState<string>('');
+    const [numeroLote, setNumeroLote] = useState<string>('');
+    const [precoCusto, setPrecoCusto] = useState<string>('');
+    const [precoVenda, setPrecoVenda] = useState<string>('');
+    const [quantidadeEstoque, setQuantidadeEstoque] = useState<string>('');
+    const [pecas, setPecas] = useState<Peca[]>([]);
+    const [editIndex, setEditIndex] = useState<number | null>(null);
+
+    const adicionarPeca = () => {
+        const novaPeca: Peca = {
+            nomePeca,
+            numeroLote,
+            precoCusto,
+            precoVenda,
+            quantidadeEstoque
+        };
+
+        if (editIndex !== null) {
+            const pecasAtualizadas = [...pecas];
+            pecasAtualizadas[editIndex] = novaPeca;
+            setPecas(pecasAtualizadas);
+            setEditIndex(null);
+        } else {
+            setPecas([...pecas, novaPeca]);
+        }
+
+        setNomePeca('');
+        setNumeroLote('');
+        setPrecoCusto('');
+        setPrecoVenda('');
+        setQuantidadeEstoque('');
+    };
+
+    const deletarPeca = (index: number) => {
+        setPecas(pecas.filter((_, i) => i !== index));
+    };
+
+    const editarPeca = (index: number) => {
+        const peca = pecas[index];
+        setNomePeca(peca.nomePeca);
+        setNumeroLote(peca.numeroLote);
+        setPrecoCusto(peca.precoCusto);
+        setPrecoVenda(peca.precoVenda);
+        setQuantidadeEstoque(peca.quantidadeEstoque);
+        setEditIndex(index);
+    };
+
     return (
         <div>
             <HeaderPage />
             <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold text-center mb-6">Gestão de Estoque de Peças</h1>
-            
-            <div className="bg-white p-4 rounded-lg shadow-md mb-6">
-                <h2 className="text-2xl font-bold mb-4">Adicionar Nova Peça</h2>
-                <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">Nome da Peça</label>
-                <input 
-                    type="text" 
-                    placeholder="Digite o nome da peça"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-                </div>
-                <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">Número do Lote</label>
-                <input 
-                    type="text" 
-                    placeholder="Digite o número do lote"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-                </div>
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                <div>
-                    <label className="block text-sm font-medium text-gray-700">Preço de Custo</label>
-                    <input 
-                    type="number" 
-                    placeholder="Digite o preço de custo"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                    />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium text-gray-700">Preço de Venda</label>
-                    <input 
-                    type="number" 
-                    placeholder="Digite o preço de venda"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                    />
-                </div>
-                </div>
-                <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">Quantidade em Estoque</label>
-                <input 
-                    type="number" 
-                    placeholder="Digite a quantidade em estoque"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-                </div>
-                <button 
-                className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-                >
-                Adicionar Peça
-                </button>
-            </div>
-            
-            <div className="bg-white p-4 rounded-lg shadow-md">
-                <h2 className="text-2xl font-bold mb-4">Lista de Peças</h2>
-                <ul>
-                <li className="mb-4 p-4 border rounded-md">
-                    <h3 className="text-xl font-bold">Nome da Peça</h3>
-                    <p><strong>Número do Lote:</strong> 123456</p>
-                    <p><strong>Preço de Custo:</strong> R$50,00</p>
-                    <p><strong>Preço de Venda:</strong> R$100,00</p>
-                    <p><strong>Quantidade em Estoque:</strong> 10 unidades</p>
-                    <div className="mt-4">
-                    <button 
-                        className="bg-yellow-500 text-white py-1 px-3 rounded-md hover:bg-yellow-600 mr-2"
-                    >
-                        Editar
-                    </button>
-                    <button 
-                        className="bg-red-500 text-white py-1 px-3 rounded-md hover:bg-red-600"
-                    >
-                        Excluir
-                    </button>
+                <h1 className="text-3xl font-bold text-center mb-6">Gestão de Estoque de Peças</h1>
+
+                <div className="bg-white p-4 rounded-lg shadow-md mb-6">
+                    <h2 className="text-2xl font-bold mb-4">{editIndex !== null ? 'Editar Peça' : 'Adicionar Nova Peça'}</h2>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Nome da Peça</label>
+                        <input 
+                            type="text" 
+                            placeholder="Digite o nome da peça"
+                            value={nomePeca}
+                            onChange={(e) => setNomePeca(e.target.value)}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
                     </div>
-                </li>
-                </ul>
-            </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Número do Lote</label>
+                        <input 
+                            type="text" 
+                            placeholder="Digite o número do lote"
+                            value={numeroLote}
+                            onChange={(e) => setNumeroLote(e.target.value)}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                        <div>
+                            <label className="block text-sm font-medium text-gray-700">Preço de Custo</label>
+                            <input 
+                                type="number" 
+                                placeholder="Digite o preço de custo"
+                                value={precoCusto}
+                                onChange={(e) => setPrecoCusto(e.target.value)}
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                            />
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium text-gray-700">Preço de Venda</label>
+                            <input 
+                                type="number" 
+                                placeholder="Digite o preço de venda"
+                                value={precoVenda}
+                                onChange={(e) => setPrecoVenda(e.target.value)}
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                            />
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Quantidade em Estoque</label>
+                        <input 
+                            type="number" 
+                            placeholder="Digite a quantidade em estoque"
+                            value={quantidadeEstoque}
+                            onChange={(e) => setQuantidadeEstoque(e.target.value)}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    <button 
+                        className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
+                        onClick={adicionarPeca}
+                    >
+                        {editIndex !== null ? 'Salvar Alterações' : 'Adicionar Peça'}
+                    </button>
+                </div>
+
+                <div className="bg-white p-4 rounded-lg shadow-md">
+                    <h2 className="text-2xl font-bold mb-4">Lista de Peças</h2>
+                    <ul>
+                        {pecas.map((peca, index) => (
+                            <li key={index} className="mb-4 p-4 border rounded-md">
+                                <h3 className="text-xl font-bold">{peca.nomePeca}</h3>
+                                <p><strong>Número do Lote:</strong> {peca.numeroLote}</p>
+                                <p><strong>Preço de Custo:</strong> R${peca.precoCusto}</p>
+                                <p><strong>Preço de Venda:</strong> R${peca.precoVenda}</p>
+                                <p><strong>Quantidade em Estoque:</strong> {peca.quantidadeEstoque} unidades</p>
+                                <div className="mt-4">
+                                    <button 
+                                        className="bg-yellow-500 text-white py-1 px-3 rounded-md hover:bg-yellow-600 mr-2"
+                                        onClick={() => editarPeca(index)}
+                                    >
+                                        Editar
+                                    </button>
+                                    <button 
+                                        className="bg-red-500 text-white py-1 px-3 rounded-md hover:bg-red-600"
+                                        onClick={() => deletarPeca(index)}
+                                    >
+                                        Excluir
+                                    </button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
--- app/dashboard/gestao-de-servicos/page.tsx
+++ app/dashboard/gestao-de-servicos/page.tsx
@@ -1,74 +1,140 @@
+"use client";
+
 import HeaderPage from "@/components/header/header";
+import { useState } from "react";
+
+type Servicos = {
+    nameService: string,
+    description: string,
+    price: string,
+    time: string
+}
 
 const CadastroDeServiços = () => {
-    return ( 
+    const [nameService, setNameService] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [price, setPrice] = useState<string>('');
+    const [time, setTime] = useState<string>('');
+    const [servicos, setServicos] = useState<Servicos[]>([]);
+    const [editIndex, setEditIndex] = useState<number | null>(null);
+
+    const adicionarServico = () => {
+        const novoServico: Servicos = {
+            nameService,
+            description,
+            price,
+            time,
+        };
+
+        if (editIndex !== null) {
+            const servicosAtualizadas = [...servicos];
+            servicosAtualizadas[editIndex] = novoServico;
+            setServicos(servicosAtualizadas);
+            setEditIndex(null);
+        } else {
+            setServicos([...servicos, novoServico]);
+        }
+
+        setNameService('');
+        setDescription('');
+        setPrice('');
+        setTime('');
+    };
+
+    const deletarServico = (index: number) => {
+        setServicos(servicos.filter((_, i) => i !== index));
+    };
+
+    const editarServico = (index: number) => {
+        const servico = servicos[index];
+        setNameService(servico.nameService);
+        setDescription(servico.description);
+        setPrice(servico.price);
+        setTime(servico.time);
+        setEditIndex(index);
+    };
+
+    return (
         <div>
             <HeaderPage />
             <div className="container mx-auto p-4">
-            <h1 className="text-3xl font-bold text-center mb-6">Gestão de Serviços</h1>
+                <h1 className="text-3xl font-bold text-center mb-6">Gestão de Serviços</h1>
             
-            <div className="bg-white p-4 rounded-lg shadow-md mb-6">
-                <h2 className="text-2xl font-bold mb-4">Adicionar Novo Serviço</h2>
-                <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">Nome do Serviço</label>
-                <input 
-                    type="text" 
-                    placeholder="Digite o nome do serviço"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-                </div>
-                <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">Descrição</label>
-                <textarea 
-                    placeholder="Digite a descrição do serviço"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-                </div>
-                <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">Preço</label>
-                <input 
-                    type="number" 
-                    placeholder="Digite o preço do serviço"
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-                </div>
-                <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">Duração Estimada</label>
-                <input 
-                    type="time" 
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                />
-                </div>
-                <button 
-                className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
-                >
-                Adicionar Serviço
-                </button>
-            </div>
-            
-            <div className="bg-white p-4 rounded-lg shadow-md">
-                <h2 className="text-2xl font-bold mb-4">Lista de Serviços</h2>
-                <ul>
-                <li className="mb-4 p-4 border rounded-md">
-                    <h3 className="text-xl font-bold">Nome do Serviço</h3>
-                    <p>Descrição do serviço...</p>
-                    <p><strong>Preço:</strong> R$100,00</p>
-                    <p><strong>Duração:</strong> 01:00</p>
-                    <div className="mt-4">
-                    <button 
-                        className="bg-yellow-500 text-white py-1 px-3 rounded-md hover:bg-yellow-600 mr-2"
-                    >
-                        Editar
-                    </button>
-                    <button 
-                        className="bg-red-500 text-white py-1 px-3 rounded-md hover:bg-red-600"
-                    >
-                        Excluir
-                    </button>
+                <div className="bg-white p-4 rounded-lg shadow-md mb-6">
+                    <h2 className="text-2xl font-bold mb-4 ">{editIndex !== null ? 'Editar Serviço' : 'Adicionar Novo Serviço'}</h2>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Nome do Serviço</label>
+                        <input
+                            onChange={(e) => setNameService(e.target.value)}
+                            value={nameService}
+                            type="text"
+                            placeholder="Digite o nome do serviço"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
                     </div>
-                </li>
-                </ul>
-            </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Descrição</label>
+                        <textarea
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder="Digite a descrição do serviço"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Preço</label>
+                        <input
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                            type="number"
+                            placeholder="Digite o preço do serviço"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-700">Duração Estimada</label>
+                        <input
+                            value={time}
+                            onChange={(e) => setTime(e.target.value)}
+                            type="time"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
+                    </div>
+                    <button
+                        className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
+                        onClick={adicionarServico}
+                    >
+                        {editIndex !== null ? 'Salvar Alterações' : 'Adicionar Serviço'}
+                    </button>
+                </div>
+            
+                <div className="bg-white p-4 rounded-lg shadow-md">
+                    <h2 className="text-2xl font-bold mb-4">Lista de Serviços</h2>
+                    <ul>
+                        {servicos.map((servico, index) => (
+                            <li key={index} className="mb-4 p-4 border rounded-md">
+                                <h3 className="text-xl font-bold">{servico.nameService}</h3>
+                                <p> {servico.description} </p>
+                                <p><strong>Preço:</strong> {servico.price} </p>
+                                <p><strong>Duração:</strong> {servico.time} </p>
+                                <div className="mt-4">
+                                    <button
+                                        onClick={() => editarServico(index)}
+                                        className="bg-yellow-500 text-white py-1 px-3 rounded-md hover:bg-yellow-600 mr-2"
+                                    >
+                                        Editar
+                                    </button>
+                                    <button
+                                        onClick={() => deletarServico(index)}
+                                        className="bg-red-500 text-white py-1 px-3 rounded-md hover:bg-red-600"
+                                    >
+                                        Excluir
+                                    </button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
